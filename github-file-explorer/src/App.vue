a131a3cea3a68357cd3490936e496f77e876b613
xiaoluoboding <xiaoluoboding@gmail.com>
2016-05-25 23:42:19 +0800

    First commit

---
+++ github-file-explorer/src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  import Github from './components/Github.vue'
+  export default {
+    name: 'App',
+    components: {
+      Github
+    }
+  }
+</script>
+
+<template>
+  <div id="app">
+    <github></github>
+  </div>
+</template>
---
+++ github-file-explorer/src/components/Github.vue
@@ -0,0 +1,48 @@
+<script>
+  import FileExplorer from './FileExplorer.vue'
+
+  export default {
+    name: 'Fullreponame',
+    data: function() {
+      return {
+        fullRepoName: '',
+        username: '',
+        repo: ''
+      };
+    },
+    methods: {
+      changeRepo: function() {
+        this.username = this.fullRepoName.split('\/').shift();
+        this.repo = this.fullRepoName.split('\/').pop();
+        console.group("Vue Data");
+        console.log("fullRepoName:", this.fullRepoName);
+        console.log("username:", this.username);
+        console.log("repo:", this.repo);
+        console.groupEnd("Vue Data");
+      }
+    },
+    components: {
+      FileExplorer
+    }
+  }
+</script>
+<template>
+  <div id="container" class="container">
+    <div class="row">
+      <h4>Github文件浏览器</h4>
+      <div class="row">
+      <form id="changeRepoForm" @submit.prevent="changeRepo()" class="col s12">
+        <div class="row">
+          <div class="col s6">
+            <label for="fullRepoName">Full Repo Name</label>
+            <input type="text" placeholder="xiaoluoboding/ghost-theme-kaldorei" id="fullRepoName" v-model="fullRepoName" class="form-control">
+          </div>
+          <div class="col s6">
+            <button type="submit" class="btn btn-primary">获&emsp;取</button>
+          </div>
+        </div>
+      </form>
+      <file-explorer :username="username" :repo="repo"></file-explorer>
+    </div>
+  </div>
+</template>
