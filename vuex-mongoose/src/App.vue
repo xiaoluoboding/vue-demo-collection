0b5baaa96dac10056f54ac4b30fc4949e29bcf41
xiaoluoboding <xiaoluoboding@gmail.com>
2016-07-28 22:31:28 +0800

    Update vuex-mongoose

---
+++ vuex-mongoose/src/App.vue
@@ -0,0 +1,37 @@
+<script>
+import store from './vuex/store'
+// import mongoose from 'mongoose'
+// import env_conf from './config/env_dev.json'
+// mongoose.connect(env_conf.db.uri, env_conf.db.options)
+// import { setModel } from './vuex/actions'
+// import User from './vuex/models/user'
+
+export default {
+  name: 'App',
+
+  store,
+
+  data () {
+    return {
+      title: 'vuex-mongoose',
+      users: []
+    }
+  },
+
+  ready () {
+    // this.setModel(User)
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <h1><span v-text="title"></span></h1>
+  </div>
+</template>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ vuex-mongoose/webpack.config.js
@@ -0,0 +1,80 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/main.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  },
+  resolve: {
+    extensions: ["", ".json", ".node", ".js"],
+    modulesDirectories: ['node_modules']
+  },
+  target: "node",
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.node$/,
+        loader: 'node'
+      },
+      {
+        test: /\.html$/,
+        loader: 'vue-html'
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ])
+}
