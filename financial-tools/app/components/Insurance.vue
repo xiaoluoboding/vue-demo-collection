a3c45a4808c48938c4f076557f85cad2ece20617
xiaoluoboding <xiaoluoboding@gmail.com>
2016-10-22 17:21:40 +0800

    Update financial-tools

--- financial-tools/app/components/Insurance.vue
+++ financial-tools/app/components/Insurance.vue
@@ -5,37 +5,63 @@
 	import XInput from 'vux-components/x-input'
 	import XButton from 'vux-components/x-button'
 	import XHeader from 'vux-components/x-header'
+	import Tab from 'vux-components/tab'
+	import TabItem from 'vux-components/tab-item'
+	import Swiper from 'vux-components/swiper'
+	import SwiperItem from 'vux-components/swiper-item'
 	import HomeTabbar from './HomeTabbar.vue'
+
+	const list = () => ['个人部分','单位部分']
 
 	export default {
 		name: 'Insurance',
-		
+
 		data () {
 			return {
 				defaultAmt: '0.00',
-				salary: ''
+				list: list(),
+				salary: '',
+				index: 0
 			}
 		},
 
 		computed: {
-			yanglao () {
+			gr_yanglao () {
 				return this.salary > 0 ? parseFloat(this.salary * 0.08).toFixed(2) : this.defaultAmt
 			},
-			yiliao () {
+			gr_yiliao () {
 				return this.salary > 0 ? parseFloat(this.salary * 0.02 + 3).toFixed(2) : this.defaultAmt
 			},
-			shiye () {
+			gr_shiye () {
 				return this.salary > 0 ? parseFloat(this.salary * 0.002).toFixed(2) : this.defaultAmt
 			},
-			gongjijin () {
+			gr_gongjijin () {
 				return this.salary > 0 ? parseFloat(this.salary * 0.12).toFixed(2) : this.defaultAmt
 			},
-			heji () {
-				return (parseFloat(this.yanglao) +
-								parseFloat(this.yiliao) +
-								parseFloat(this.shiye) +
-								parseFloat(this.gongjijin)).toFixed(2)
-			}
+			gr_heji () {
+				return (parseFloat(this.gr_yanglao) + parseFloat(this.gr_yiliao) + parseFloat(this.gr_shiye) + parseFloat(this.gr_gongjijin)).toFixed(2)
+			},
+			dw_yanglao () {
+				return this.salary > 0 ? parseFloat(this.salary * 0.2).toFixed(2) : this.defaultAmt
+			},
+			dw_yiliao () {
+				return this.salary > 0 ? parseFloat(this.salary * 0.1).toFixed(2) : this.defaultAmt
+			},
+			dw_shiye () {
+				return this.salary > 0 ? parseFloat(this.salary * 0.01).toFixed(2) : this.defaultAmt
+			},
+			dw_gongshang () {
+				return this.salary > 0 ? parseFloat(this.salary * 0.01).toFixed(2) : this.defaultAmt
+			},
+			dw_shengyu () {
+				return this.salary > 0 ? parseFloat(this.salary * 0.008).toFixed(2) : this.defaultAmt
+			},
+			dw_heji () {
+				return (
+					parseFloat(this.dw_yanglao) + parseFloat(this.dw_yiliao) +
+					parseFloat(this.dw_shiye) + parseFloat(this.dw_gongshang) +
+					parseFloat(this.dw_shengyu) + parseFloat(this.gr_gongjijin)).toFixed(2)
+			},
 		},
 
 	  components: {
@@ -45,6 +71,10 @@
 	    XInput,
 	    XButton,
 	    XHeader,
+			Tab,
+			TabItem,
+			Swiper,
+			SwiperItem,
 	    HomeTabbar
 	  }
 	}
@@ -61,7 +91,43 @@
 		  	text-align="right"></x-input>
 		</group>
 
-		<group title="个人缴存部分">
+		<tab :line-width=2 active-color='#fc378c' :index.sync="index">
+      <tab-item class="vux-center" :selected="tab1 === item" v-for="item in list" @click="tab1 = item">{{item}}</tab-item>
+    </tab>
+    <swiper :index.sync="index" height="480px" :show-dots="false">
+      <swiper-item>
+				<group>
+				  <cell title="养老" :value="gr_yanglao"></cell>
+				  <cell title="医疗" :value="gr_yiliao"></cell>
+				  <cell title="失业" :value="gr_shiye"></cell>
+				  <cell title="工伤" :value="gr_gongshang"></cell>
+				  <cell title="生育" :value="gr_shengyu"></cell>
+				  <cell title="公积金" :value="gr_gongjijin"></cell>
+				  <cell title="合计">
+		        <div slot="value">
+		          <span style="color: green" v-text="gr_heji"></span>
+		        </div>
+		      </cell>
+				</group>
+      </swiper-item>
+      <swiper-item>
+				<group>
+				  <cell title="养老" :value="dw_yanglao"></cell>
+				  <cell title="医疗" :value="dw_yiliao"></cell>
+				  <cell title="失业" :value="dw_shiye"></cell>
+				  <cell title="工伤" :value="dw_gongshang"></cell>
+				  <cell title="生育" :value="dw_shengyu"></cell>
+				  <cell title="公积金" :value="gr_gongjijin"></cell>
+				  <cell title="合计">
+		        <div slot="value">
+		          <span style="color: green" v-text="dw_heji"></span>
+		        </div>
+		      </cell>
+				</group>
+      </swiper-item>
+    </swiper>
+
+		<!-- <group title="个人缴存部分">
 		  <cell title="养老" :value="yanglao"></cell>
 		  <cell title="医疗" :value="yiliao"></cell>
 		  <cell title="失业" :value="shiye"></cell>
@@ -73,8 +139,7 @@
           <span style="color: green" v-text="heji"></span>
         </div>
       </cell>
-		  <!--  -->
-		</group>
+		</group> -->
 	  <home-tabbar></home-tabbar>
 	</div>
 </template>
